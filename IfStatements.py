51e5dd2fcf24c3f3397939957f81ee522f17f5a0
EmmaSavin <154214967+EmmaSavin@users.noreply.github.com>
2024-04-24 14:20:55 +0100

    Add files via upload

---
+++ IfStatements.py
@@ -0,0 +1,17 @@
+num = int(input("Enter a number: "))
+
+if num > 10:
+    print("This is over10")
+elif num == 10:
+    print("This is equal to 10")
+else:
+    print("This is not over 10")
+    
+if num >= 10:
+    if num <= 20:
+        print("This is between 10 and 20")
+    else:
+        print("This is over 20")
+else:
+    print("This is under 20")
+    
---
+++ stringManipulation.py
@@ -0,0 +1,21 @@
+name = input("Enter Name: ")
+for letter in name:
+    print(letter,end="*")
+
+surname = input(" Enter Surname: ")
+for letter in surname:
+    print(letter,end="*")
+
+print( name, surname)
+
+
+"""
+if fullname.isupper():
+    print("Uppercase")
+else:
+    print(" This is not in uppercase") 
+if fullname.islower():
+    print("Lowercase")
+else:
+    print(" This is not in lowercase")
+"""
